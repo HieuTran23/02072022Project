48cd48e7a263085e68c81c6de2b76053ef2a1294
trinhthehoa <trinhthehoa2001@gmail.com>
2022-03-05 23:54:16 +0700

    Update profile.js

--- routes/admin/profile.js
+++ routes/admin/profile.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const argon2 = require("argon2")
 const User = require('../../models/user')
 const Role = require('../../models/role');
 const Role = require('../../models/profile');
