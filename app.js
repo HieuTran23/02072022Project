c87a5c6f58514654910d1d4558949602f78dad0b
Hieu <tdhieu23@gmail.com>
2022-03-07 17:28:02 +0700

    Update app.js

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.use('/', authRouter)
 app.use('/profile', profileRouter)
 
 //--|--Submission
-app.use('submission', submissionRouter)
+app.use('/submission', submissionRouter)
 
 //--User end
 /**
