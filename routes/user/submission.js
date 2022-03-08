73305e7e4650de013481025609529be5b18aaf95
Hieu <tdhieu23@gmail.com>
2022-03-09 01:08:23 +0700

    fix

--- routes/user/submission.js
+++ routes/user/submission.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const router = express.Router();
-const Submission = require('../../models/submission')
+const submissionRouter = require('../../models/submission')
 
 //View profile list 
 router.get('/' , async (req ,res) => {
     try {
-        const submissions = await Submission.find();
-
-        res.render('pages/user/submission', {
-            title: 'View',
-            page: 'Submission',
-            submissions
+        const user = await User.findOne({
+            _id :req.params.id
         })
+        const roles = await Role.find()
+        const departments = await Department.find()
 
+        res.render('pages/user/profile',{
+            title: 'Profile',
+            page: 'User',
+            user,
+            roles,
+            departments
+        })
     } catch (error) {
         console.log(error)
         res.status(500) .json({success:false , message:'Error'}) 
