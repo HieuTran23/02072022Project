8773bbb9247633a5f5109bcde5c6269d13454e63
Hieu <tdhieu23@gmail.com>
2022-03-09 01:20:00 +0700

    fix

--- app.js
+++ app.js
@@ -7,30 +7,19 @@ const mongoose = require('mongoose')
 var bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser")
 const path = require('path')
-<<<<<<< HEAD
-=======
 const Role = require('./models/role')
 
->>>>>>> parent of a2b572f (Merge branch 'category')
-const department = require('./models/department')
 //--Router
 //--|--Admin
 const authRouter = require('./routes/auth')
 const adminRouter = require('./routes/admin')
-<<<<<<< HEAD
+
 const userRouter = require('./routes/admin/user');
-const roleRouter = require('./routes/admin/role')
-const submissionRouter = require('./routes/admin/submission')
-const categoryRouter = require('./routes/admin/category')
-const userAdminRouter = require('./routes/admin/user');
-=======
-const userAdminRouter = require('./routes/admin/user');
 const roleAdminRouter = require('./routes/admin/role')
 const submissionAdminRouter = require('./routes/admin/submission')
->>>>>>> parent of a2b572f (Merge branch 'category')
-const departmentAdminRouter = require('./routes/admin/department')
+const userAdminRouter = require('./routes/admin/user');
+
 //--|--User
-const profileRouter = require('./routes/user/profile')
 const submissionRouter = require('./routes/user/submission')
 //--
 /**
@@ -75,26 +64,9 @@ app.use('/admin', adminRouter)
 //--|--Role
 app.use('/admin/role', roleAdminRouter)
 
-//--|--department
-app.use('/admin/department',departmentAdminRouter)
-
-<<<<<<< HEAD
-//submission
-app.use('/admin/submission',submissionRouter)
-
-//academic year
-//Account
-//app.use('/api/account', accountRouter)
-
 //--|--submission
 app.use('/admin/submission',submissionAdminRouter)
 
-
-=======
-//--|--submission
-app.use('/admin/submission',submissionAdminRouter)
-
->>>>>>> parent of a2b572f (Merge branch 'category')
 //--|--User
 app.use('/admin/user', userAdminRouter)
 //--Admin end
@@ -104,8 +76,6 @@ app.use('/', authRouter)
 //--Auth end 
 
 //--User start
-//--|--profile
-app.use('/profile', profileRouter)
 
 //--|--Submission
 app.use('/submission', submissionRouter)
