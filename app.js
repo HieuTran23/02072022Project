de91eb79f77cdf449eb463169d278d0b297ec8d0
Hieu <tdhieu23@gmail.com>
2022-03-09 01:02:47 +0700

    fix

--- app.js
+++ app.js
@@ -7,43 +7,20 @@ const mongoose = require('mongoose')
 var bodyParser = require('body-parser');
 const cookieParser = require("cookie-parser")
 const path = require('path')
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
-const Role = require('./models/role')
->>>>>>> parent of 14e5ad2 (Revert "Merge branch 'category'")
-=======
-const Role = require('./models/role')
-<<<<<<< HEAD
->>>>>>> parent of aef08d0 (Merge branch 'main' of https://github.com/HieuTran23/02072022Project)
-=======
-
 const department = require('./models/department')
->>>>>>> parent of a2b572f (Merge branch 'category')
 //--Router
 //--|--Admin
 const authRouter = require('./routes/auth')
 const adminRouter = require('./routes/admin')
-<<<<<<< HEAD
 const userRouter = require('./routes/admin/user');
 const roleRouter = require('./routes/admin/role')
 const submissionRouter = require('./routes/admin/submission')
-<<<<<<< HEAD
 const categoryRouter = require('./routes/admin/category')
-=======
->>>>>>> parent of aef08d0 (Merge branch 'main' of https://github.com/HieuTran23/02072022Project)
-
-const profileUser = require('./routes/user/index')
-=======
 const userAdminRouter = require('./routes/admin/user');
-const roleAdminRouter = require('./routes/admin/role')
-const submissionAdminRouter = require('./routes/admin/submission')
 const departmentAdminRouter = require('./routes/admin/department')
 //--|--User
 const profileRouter = require('./routes/user/profile')
 const submissionRouter = require('./routes/user/submission')
->>>>>>> parent of a2b572f (Merge branch 'category')
 //--
 /**
  * App Variables
@@ -84,44 +61,27 @@ app.use(express.static('public'))
 //--|--Dashboard
 app.use('/admin', adminRouter)
 
-<<<<<<< HEAD
 //Role
 app.use('/admin/role', roleRouter)
-<<<<<<< HEAD
+
 
 //-----User
 app.use('/user/profile',profileUser)
-=======
-//--|--Role
-app.use('/admin/role', roleAdminRouter)
->>>>>>> parent of a2b572f (Merge branch 'category')
 
 //--|--department
 app.use('/admin/department',departmentAdminRouter)
 
-<<<<<<< HEAD
 //submission
 app.use('/admin/submission',submissionRouter)
  //category
 app.use('/admin/category',categoryRouter)
-=======
-
-//-----User
-app.use('/user/profile',profileUser)
-
-//--Admin
-
-//submission
-app.use('/admin/submission',submissionRouter)
-
->>>>>>> parent of aef08d0 (Merge branch 'main' of https://github.com/HieuTran23/02072022Project)
 //academic year
 //Account
 //app.use('/api/account', accountRouter)
-=======
+
 //--|--submission
 app.use('/admin/submission',submissionAdminRouter)
->>>>>>> parent of a2b572f (Merge branch 'category')
+
 
 //--|--User
 app.use('/admin/user', userAdminRouter)
