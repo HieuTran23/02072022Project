04d18884cab0ba70d2f579b2e87aff9bc53ca85b
Hieu <tdhieu23@gmail.com>
2022-03-08 19:38:56 +0700

    Idea schema

---
+++ models/idea.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ideaSchema = new Schema({
+    title: {
+        type: String,
+        unique: true,
+        require: true,
+        max: 255
+    },
+    category: {
+        type: mongoose.Types.ObjectId,
+        ref: 'category'
+    },
+    description: {
+        type: String,
+    },
+    content: {
+        type: String
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'user'
+    },
+    submission: {
+        type: mongoose.Types.ObjectId,
+        ref: 'submission'
+    },
+    lastModifiedAt: [{
+        type: Date,
+        default: Date.now
+    }],
+    createAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('idea', ideaSchema)
