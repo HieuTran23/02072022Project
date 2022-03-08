71e1446883634c03b4a6ed3f5bddbed53b4c1381
Hieu <tdhieu23@gmail.com>
2022-03-09 00:54:44 +0700

    Revert "Merge branch 'Template'"
    This reverts commit 8e4dd88d2cdf19d9bcf1608a3dd2f152704b77b6, reversing
    changes made to 04d18884cab0ba70d2f579b2e87aff9bc53ca85b.

--- app.js
+++ app.js
@@ -15,10 +15,17 @@ const Role = require('./models/role')
 >>>>>>> parent of 14e5ad2 (Revert "Merge branch 'category'")
 =======
 const Role = require('./models/role')
+<<<<<<< HEAD
 >>>>>>> parent of aef08d0 (Merge branch 'main' of https://github.com/HieuTran23/02072022Project)
+=======
+
+const department = require('./models/department')
+>>>>>>> parent of a2b572f (Merge branch 'category')
 //--Router
+//--|--Admin
 const authRouter = require('./routes/auth')
 const adminRouter = require('./routes/admin')
+<<<<<<< HEAD
 const userRouter = require('./routes/admin/user');
 const roleRouter = require('./routes/admin/role')
 const submissionRouter = require('./routes/admin/submission')
@@ -28,6 +35,15 @@ const categoryRouter = require('./routes/admin/category')
 >>>>>>> parent of aef08d0 (Merge branch 'main' of https://github.com/HieuTran23/02072022Project)
 
 const profileUser = require('./routes/user/index')
+=======
+const userAdminRouter = require('./routes/admin/user');
+const roleAdminRouter = require('./routes/admin/role')
+const submissionAdminRouter = require('./routes/admin/submission')
+const departmentAdminRouter = require('./routes/admin/department')
+//--|--User
+const profileRouter = require('./routes/user/profile')
+const submissionRouter = require('./routes/user/submission')
+>>>>>>> parent of a2b572f (Merge branch 'category')
 //--
 /**
  * App Variables
@@ -65,18 +81,25 @@ app.use(express.static('public'))
  */
 
 //--Admin start
-//Dashboard
+//--|--Dashboard
 app.use('/admin', adminRouter)
 
+<<<<<<< HEAD
 //Role
 app.use('/admin/role', roleRouter)
 <<<<<<< HEAD
 
 //-----User
 app.use('/user/profile',profileUser)
+=======
+//--|--Role
+app.use('/admin/role', roleAdminRouter)
+>>>>>>> parent of a2b572f (Merge branch 'category')
 
-//--Admin
+//--|--department
+app.use('/admin/department',departmentAdminRouter)
 
+<<<<<<< HEAD
 //submission
 app.use('/admin/submission',submissionRouter)
  //category
@@ -95,10 +118,13 @@ app.use('/admin/submission',submissionRouter)
 //academic year
 //Account
 //app.use('/api/account', accountRouter)
+=======
+//--|--submission
+app.use('/admin/submission',submissionAdminRouter)
+>>>>>>> parent of a2b572f (Merge branch 'category')
 
-
-//User
-app.use('/admin/user', userRouter)
+//--|--User
+app.use('/admin/user', userAdminRouter)
 //--Admin end
 
 //--Auth start
@@ -106,6 +132,11 @@ app.use('/', authRouter)
 //--Auth end 
 
 //--User start
+//--|--profile
+app.use('/profile', profileRouter)
+
+//--|--Submission
+app.use('/submission', submissionRouter)
 
 //--User end
 /**
