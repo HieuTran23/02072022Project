2fc31f8a7816db10a2cd7b4f33844e58fe62ec18
tuanhuy0211 <huyhvtgcs190271@fpt.edu.vn>
2022-03-11 13:23:01 +0700

    setting profile

--- routes/user/setting.js
+++ routes/user/setting.js
@@ -1,17 +1,25 @@
 const express = require("express");
-const role = require("../../models/role");
+
 const router = express.Router();
-const User = require('../../models/user');
+
+const User = require('../../models/user')
 const Role = require('../../models/role');
+const Department = require('../../models/department')
 
 //Edit and update user
 //-- get
 router.get('/:id', async (req, res) => {
     try {
-		const getUser = await User.findOne({
-            _id : req.params.id
+		const user = await User.findById({ _id: req.params.id})
+        const roles = await Role.find()
+        const departments = await Department.find()
+        res.render('pages/user/setting', {
+            title: "Edit",
+            page: "User",
+            user,
+            roles,
+            departments
         })
-        res.json(getUser)
     }catch (error) {
         console.log(error)
         res.status(500).json({success:false , message:'Error', error})
@@ -20,7 +28,7 @@ router.get('/:id', async (req, res) => {
 
 //-- Post 
 router.post('/:id' , async(req,res)=>{
-    const {fullName, emails, phones, streets, cities, countries} = req.body
+    const {fullName, departmentId,emails, phones, streets, cities, countries} = req.body
      //validation
     try {
         //Create contact field (object)
@@ -58,12 +66,13 @@ router.post('/:id' , async(req,res)=>{
         let editUser = {
             fullName: fullName,
             contact: contact,
+            departmentId
         }
-        res.json({success : true ,message:'edit successful' , edit : editUser})
+        
         //Update user to database
         const updatedUser = await User.findOneAndUpdate({_id: req.params.id}, editUser, {new: true})
-
-        
+        var id= req.params.id;
+        res.redirect('/setting/'+ id)
 
         
     } catch (error) { 
