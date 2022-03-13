680504b9097a2b5ce33ba5b5653c701898561527
Hieu <tdhieu23@gmail.com>
2022-03-13 15:06:46 +0700

    fix setting-password

--- template/user/js/main.js
+++ template/user/js/main.js
@@ -302,17 +302,19 @@
           var $bigLetter = $(".big-letter");
           var $num = $(".num");
           var $specialChar = $(".special-char");
+          var $equal = $(".equal")
           var specialChars = "!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?`~";
           var numbers = "0123456789";
       
           $requirements.addClass("wrong");
           $password.on("focus", function(){$passwordAlert.show();});
-      
+          $rePassword.on("focus", function(){$passwordAlert.show();});
+
           $password.on("input blur", function (e) {
               var el = $(this);
               var val = el.val();
               $passwordAlert.show();
-      
+            
               if (val.length < 8) {
                   leng = false;
               }
@@ -345,7 +347,7 @@
               }
 
               equal=false;
-              if($password === $rePassword) equal = true;
+              if($password.val() == $rePassword.val()) {equal = true};
       
               console.log(leng, bigLetter, num, specialChar);
               
@@ -373,13 +375,86 @@
 
                   if(equal==false){$equal.addClass("wrong").removeClass("good");}
                   else{$equal.addClass("good").removeClass("wrong");}
-              }
+              } 
               
               
               if(e.type == "blur"){
                       $passwordAlert.hide();
-                  }
+              }
           });
+
+          $rePassword.on("input blur", function (e) {
+            var el = $password;
+            var val = el.val();
+            $passwordAlert.show();
+          
+            if (val.length < 6) {
+                leng = false;
+            }
+            else if (val.length > 7) {
+                leng=true;
+            }
+            
+    
+            if(val.toLowerCase()==val){
+                bigLetter = false;
+            }
+            else{bigLetter=true;}
+            
+            num = false;
+            for(var i=0; i<val.length;i++){
+                for(var j=0; j<numbers.length; j++){
+                    if(val[i]==numbers[j]){
+                        num = true;
+                    }
+                }
+            }
+            
+            specialChar=false;
+            for(var i=0; i<val.length;i++){
+                for(var j=0; j<specialChars.length; j++){
+                    if(val[i]==specialChars[j]){
+                        specialChar = true;
+                    }
+                }
+            }
+
+            equal=false;
+            if($password.val() == $rePassword.val()) {equal = true};
+    
+            console.log(leng, bigLetter, num, specialChar);
+            
+            if(leng==true&&bigLetter==true&&num==true&&specialChar==true&&equal==true){
+                $(this).addClass("valid").removeClass("invalid");
+                $requirements.removeClass("wrong").addClass("good");
+                $passwordAlert.removeClass("alert-warning").addClass("alert-success");
+            }
+            else
+            {
+                $(this).addClass("invalid").removeClass("valid");
+                $passwordAlert.removeClass("alert-success").addClass("alert-warning");
+    
+                if(leng==false){$leng.addClass("wrong").removeClass("good");}
+                else{$leng.addClass("good").removeClass("wrong");}
+    
+                if(bigLetter==false){$bigLetter.addClass("wrong").removeClass("good");}
+                else{$bigLetter.addClass("good").removeClass("wrong");}
+    
+                if(num==false){$num.addClass("wrong").removeClass("good");}
+                else{$num.addClass("good").removeClass("wrong");}
+    
+                if(specialChar==false){$specialChar.addClass("wrong").removeClass("good");}
+                else{$specialChar.addClass("good").removeClass("wrong");}
+
+                if(equal==false){$equal.addClass("wrong").removeClass("good");}
+                else{$equal.addClass("good").removeClass("wrong");}
+            } 
+            
+            
+            if(e.type == "blur"){
+                    $passwordAlert.hide();
+            }
+        });
       });
       }
     })
