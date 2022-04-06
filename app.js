354916e90bfa5673dadeed5b1442a2c1af810f74
Hieu <tdhieu23@gmail.com>
2022-04-06 23:13:47 +0700

    Update

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ const app = express();
  *  App Configuration
  */
 // Database config
-mongoose.connect('mongodb+srv://admin:admin@cluster.ur4xo.mongodb.net/Database?retryWrites=true&w=majority', (err, db) => {
+mongoose.connect(process.env.MONGO_URI, (err, db) => {
     if (err) console.log(err);
     console.log('Connect database success')
 })
@@ -115,7 +115,7 @@ app.use('/', uploadRouter)
 /**
  * Server Activation
  */
-const port = 5000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
     console.log('Server running on port ' + port);
 })
